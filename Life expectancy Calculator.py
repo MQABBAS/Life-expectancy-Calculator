02d07384510c36b36133e0eab2de19766403dd01
Mohammed Abbas <101219974+MQABBAS@users.noreply.github.com>
2022-03-11 18:50:40 +0000

    Add files via upload

---
+++ Life expectancy Calculator.py
@@ -0,0 +1,11 @@
+print('check your life prediction now!')
+age = input('What is your age?')
+Should_be = 81
+Ans = int(Should_be) - int(age)
+Ans1 = int(Ans)
+print(Ans1)
+
+if Ans < 81:
+    print('You have That Much years to live ')
+
+
